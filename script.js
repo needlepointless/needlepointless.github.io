ddedfdb6b4c5fe094d13b804dac59bf5400664eb
Person's Name <artofcoded@gmail.com>
2019-11-06 22:07:46 -0500

    Implemented stupid post system

---
+++ script.js
@@ -0,0 +1,22 @@
+const loadJSON = (filePath, success, error) => {
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = () => {
+		if (xhr.readyState === XMLHttpRequest.DONE) {
+			if (xhr.status === 200 && success) success(JSON.parse(xhr.responseText));
+		} else if (error) error(xhr);
+	};
+	xhr.open("GET", filePath, true);
+	xhr.send();
+}
+
+loadJSON("./data.json",(t) => {
+    let cont = ""
+    
+    t.posts.forEach((i) => {
+
+        cont += `<div class="post"><h3>${i.title}</h3>${i.body}</div>`
+
+    })
+
+    document.getElementById("page-content").innerHTML = cont
+})
